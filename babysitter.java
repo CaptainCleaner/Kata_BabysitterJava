b1ac02cc8c4c7c07b55b5fb8a2c660d79a1cbf93
CaptainCleaner <bostonweber@gmail.com>
2016-09-01 15:27:08 -0400

    Add files via upload

---
+++ babysitter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class babysitter  {
+    public static void main(String[] args) {
+        int rate1 = 3;
+        int rate2 = 4;
+        int hrate1 = 12;
+        int hrate2 = 8;
+        int hrate3 = 16;
+        
+        System.out.println((rate1 * hrate1) + (rate2 * hrate2) + (rate2 * hrate3));
+    }
+}
